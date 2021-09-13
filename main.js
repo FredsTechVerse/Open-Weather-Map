6df43dee4683d06d218169fce61a6647f026faae
Alfred <alfredgithinji87@gmail.com>
2021-09-13 18:07:19 +0300

    Challenge complete

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+//GROCERY STORE
+const city = document.querySelector('#city');
+const longitude = document.querySelector('#longitude');
+const latitude = document.querySelector('#latitude');
+const temperature = document.querySelector('#temperature');
+const humidity = document.querySelector('#humidity');
+const pressure = document.querySelector('#pressure');
+
 class Weather{
+
+ //   METHOD 1(Destructures Data from the weather API)  
+//  ============ 
 async getProducts(){
     try {
 
-        let rawData = fetch("weather.json");
+        let rawData = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Limuru&appid=567766cda2201c50960c8f37b2ee3bf8");
 
-        let data = await (await rawData).json();
+        let data = await rawData.json();
 
         return data;
         
@@ -17,7 +28,8 @@ async getProducts(){
 }
 
 
-
+//  METHOD 2(Destructures the data into a simpler object )
+// ===========
 consumeData(data){
  
    let {lon:longitude, lat:latitude} = data.coord;
@@ -26,20 +38,40 @@ consumeData(data){
    return {city ,longitude , latitude , temp , pressure , humidity};
 }
 
+//  METHOD 3(Saves the data as an object)
+// ===========
+saveData (data){
+    let newObject = this.consumeData(data);
+     this.displayData(newObject);
+}
 
-displayData (data){
-    let newObject= this.consumeData(data);
 
-    console.log(newObject);
+
+displayData(newObject){
+    city.innerHTML = newObject.city;
+    longitude.innerHTML = newObject.longitude;
+    latitude.innerHTML = newObject.latitude;
+    temperature.innerHTML = newObject.temp;
+    humidity.innerHTML = newObject.humidity;
+    pressure.innerHTML = newObject.pressure;
+
+
+
+    
+
+    
+
+  
 }
 }
 
 
 
-
+// ClASS INSTANTIATION
+//====================
 
 let weather = new Weather ();
 
-weather.getProducts().then((data)=>{
-    weather.displayData(data);
+weather.getProducts().then((data)=>{//Data is the name given to the return value(resolve or reject data).
+    weather.saveData(data);
 })
